4815164a8eefbe32c5f791fc628a36b3011faaaf
Mahomed Akhmedov <akhmedovmagomed2001@gmail.com>
2025-02-15 22:49:05 +0200

    [Project] Clean sources

--- app/routes.ts
+++
@@ -1,9 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-  index("routes/home.tsx"),
-  route("/login", "routes/login.tsx"),
-  route("/transfer", "routes/transfer.tsx"),
-  route("/medicine", "routes/medicine.tsx"),
-  route("/social", "routes/social.tsx"),
-] satisfies RouteConfig;
