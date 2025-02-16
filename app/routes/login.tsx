736a7f5fec3a8def9be75a525282f300d72a35d5
Mahomed Akhmedov <akhmedovmagomed2001@gmail.com>
2025-02-16 04:55:12 +0200

    [LoginPage] Implement

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -10,8 +10,51 @@ export function meta({}: Route.MetaArgs) {
 export default function Login() {
   return (
     <>
-      <h1>Login page</h1>
-      <p>Please enter your login and password</p>
+      <div className="mx-5 mt-25 mb-10 w-max mx-auto">
+        <div className="flex flex-col gap-0">
+          <h1 className="text-2xl/5 font-semibold">Ride it.</h1>
+          <h2 className="text-2xl font-semibold text-gray-400">
+            Login to your account
+          </h2>
+        </div>
+        <hr className="h-px my-5 bg-gray-200 border-0 dark:bg-gray-2"></hr>
+        <form action="#">
+          <div>
+            <label className="text-sm text-gray-600" htmlFor="email">
+              Email
+            </label>
+            <div>
+              <input
+                className="border-1 border-gray-200 rounded-lg w-full px-2 py-2.5 text-sm"
+                type="text"
+                id="email"
+                placeholder="Enter your email address..."
+              />
+            </div>
+          </div>
+          <div className="mt-4">
+            <label className="text-sm text-gray-600" htmlFor="password">
+              Password
+            </label>
+            <div>
+              <input
+                className="border-1 border-gray-200 rounded-lg w-full px-2 py-2.5 text-sm"
+                type="password"
+                id="password"
+                placeholder="Enter your password..."
+              />
+            </div>
+          </div>
+          <div className="text-center">
+            <button
+              className="mt-10 w-full inline-flex rounded-lg py-4 text-sm font-semibold transition bg-neutral-950 text-center text-white hover:bg-sky-400"
+              type="submit"
+            >
+              <div className="w-full mx-auto text-base">Login</div>
+            </button>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
