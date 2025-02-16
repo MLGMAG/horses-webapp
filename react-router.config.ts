43ad0e06bb46d2c2b54160dbd2168dae1a7b6a60
Mahomed Akhmedov <akhmedovmagomed2001@gmail.com>
2025-02-16 05:00:25 +0200

    [LoginPage] Add to prerender

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@react-router/dev/config";
 export default {
   ssr: false,
   async prerender() {
-    return ["/", "/transfer", "/medicine", "/social"];
+    return ["/", "/transfer", "/medicine", "/social", "/login"];
   },
   basename: "/horses-webapp/",
 } satisfies Config;
