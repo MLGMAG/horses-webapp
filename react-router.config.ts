0ddc0a8bf66e7818c0f156e36c720791176400db
Mahomed Akhmedov <akhmedovmagomed2001@gmail.com>
2025-03-03 10:51:38 +0200

    [Render] Prerender 'market' route

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@react-router/dev/config";
 export default {
   ssr: false,
   async prerender() {
-    return ["/", "/logistics", "/medicine", "/social", "/login"];
+    return ["/", "/logistics", "/medicine", "/social", "/market", "/login"];
   },
   basename: "/horses-webapp/",
 } satisfies Config;
