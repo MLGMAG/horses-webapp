246bc669ff09cc61267d3cccff6d76fb6ad53d27
Mahomed Akhmedov <akhmedovmagomed2001@gmail.com>
2025-03-20 23:35:51 +0200

    [Hydration] Remove

--- app/root.tsx
+++ app/root.tsx
@@ -74,12 +74,3 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
     </main>
   );
 }
-
-export function HydrateFallback() {
-  return (
-    <div id="loading-splash">
-      <div id="loading-splash-spinner" />
-      <p>Loading, please wait...</p>
-    </div>
-  );
-}
